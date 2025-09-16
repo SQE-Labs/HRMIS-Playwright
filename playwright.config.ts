88af2e1f91e171f66f9fabb479388bae0c92d321
ArchitSQE <archit.khurana@sqelabs.com>
2025-09-16 18:55:10 +0530

    Added @smoke tag

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
   //   }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false, // to see the browser window
+    headless: true, // to see the browser window
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
