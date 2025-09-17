274f3afc61e4496a1d285d914e76586b8e8ab605
ArchitSQE <archit.khurana@sqelabs.com>
2025-09-17 16:27:37 +0530

    email

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   //  "LoginPage.spec.ts"
   // ],
   fullyParallel: false,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   // workers: process.env.CI ? 1 : undefined,
   workers: 4,
   reporter: [["html"], ['line'], ["allure-playwright"], ["./my-reporter.ts"], ['json', { outputFile: 'playwright-report/results.json' }]],
