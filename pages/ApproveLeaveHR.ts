e1824af976b5c5f1cee50051d12eeedd2954a5b4
Preeti1484 <preeti@sqelabs.com>
2025-09-22 17:30:22 +0530

    New PR

--- pages/ApproveLeaveHR.ts
+++ pages/ApproveLeaveHR.ts
@@ -1,19 +1,19 @@
-import { LoginPage } from '../pages/LoginPage';
-import { BasePage } from '../pages/Basepage';
-import { expect, Locator, Page } from '@playwright/test';
+// import { LoginPage } from '../pages/LoginPage';
+// import { BasePage } from '../pages/Basepage';
+// import { expect, Locator, Page } from '@playwright/test';
 
-export class ApproveLeaveHR extends BasePage {
+// export class ApproveLeaveHR extends BasePage {
 
 
-    
-    constructor(page: Page) {{
-        super(page);
-        private page: Page;
+
+//     constructor(page: Page) {{
+//         super(page);
+//         private page: Page;
    
-    }
+//     }
     
 
-}
+// }
 
 
 
