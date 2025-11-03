3e4d96bb52e9d145a13cc8c0225fd5f711895d2b
Preeti <preeti@sqelabs.com>
2025-11-03 17:50:26 +0530

    Added to test cases of Salary Management

--- tests/SalaryManagement.spec.ts
+++ tests/SalaryManagement.spec.ts
@@ -5,6 +5,7 @@ import testData from '../testData/testData.json';
 import { SalaryManagement } from '../pages/SalaryManagement';
 import { CommonUtils } from '../utils/commonUtils';
 import { Helper } from '../utils/Helper';
+import * as constants from "../utils/constants";
 
 let salaryObj: SalaryManagement
 let utils: CommonUtils;
@@ -36,5 +37,37 @@ test.describe.serial("Salary Management", () => {
         const successToast = await salaryObj.toastMessage();
         await expect(successToast).toEqual("Successfully Uploaded!");
     });
+
+    test ("Verify the Reset functionality @reg, @eti", async({page})=>{
+        await salaryObj.selectEmployeeType("REGULAR");
+        await utils.uploadAndVerifyFile(testData.EXISTINGSERIALNUMBER_COLUMN, page, salaryObj.submitButton);
+        await salaryObj.selectMonth("August");
+        await salaryObj.resetButton.click();
+
+        await page.waitForLoadState()
+
+        const resetEmployeeType = await salaryObj.employeeTypeDropdown.inputValue();
+        const resetMonth = await salaryObj.monthDropdown.inputValue();
+
+        // Assert the fields are cleared / reset
+        expect(resetEmployeeType).toBe("");
+        expect(resetMonth).toBe(""); 
+    })
+
+    test("Verify the validation tooltip of Salary Management Tab @reg, @eti", async({page})=>{
+        // step 1: Leaving all field blank and getting validation tooltip
+        await salaryObj.submitForm();
+        await salaryObj.verifyTooltipMessage(salaryObj.employeeTypeDropdown, constants.PLEASE_SELECT_ITEM_TOOLTIP)
+
+        // step 2: Verify the tooltip for salary slip field 
+        await salaryObj.selectEmployeeType("REGULAR");
+        await salaryObj.submitForm();
+        await salaryObj.verifyTooltipMessage(salaryObj.salarySlipUpload, constants.PLEASE_SELECT_FILE_TOOLTIP)
+
+        // step 3 verify tooltip for select month field 
+        await utils.uploadAndVerifyFile(testData.EXISTINGSERIALNUMBER_COLUMN, page, salaryObj.submitButton);
+        await salaryObj.submitForm();
+        await salaryObj.verifyTooltipMessage(salaryObj.monthDropdown, constants.PLEASE_SELECT_ITEM_TOOLTIP)
+    })
 
 });
