3ddad4fc8b83458b004d62b245d03643171f275a
Yogesh <yogesh@sqelabs.com>
2026-06-25 18:41:34 +0530

    added test case for timesheet rejection

--- pages/PendingApprovals.ts
+++ pages/PendingApprovals.ts
@@ -4,11 +4,21 @@ import { Page, Locator, ElementHandle } from "@playwright/test";
 export class PendingApprovals extends BasePage{
     private viewButton : Locator;
     private approveButton : Locator;
+    private rejectButton : Locator;
+    private commentBox : Locator;
+    private allowResubmissionDialog : Locator;
+    private resubmissionDialogHeading : Locator;
+    private resubmissionAllowButton : Function;
     
     constructor(page: Page){
         super(page);
         this.viewButton = page.locator("(//a[text()='View'])[1]");
         this.approveButton = page.getByRole("button", { name : "Approve"});
+        this.rejectButton = page.getByRole("button", { name : "Reject"});
+        this.commentBox = page.getByPlaceholder("Add comment or reason (required for reject)...");
+        this.allowResubmissionDialog = page.getByRole('dialog').nth(1);
+        this.resubmissionDialogHeading = this.allowResubmissionDialog.getByRole("heading");
+        this.resubmissionAllowButton = (option : string) => this.allowResubmissionDialog.getByRole("button", { name : `${option}`});
     }
 
     public async clickOnViewButtonforFirstResult(){
@@ -21,6 +31,30 @@ export class PendingApprovals extends BasePage{
         while(await this.approveButton.count() > 0){
             await eHandles[index].click();
             index++;
+        }
+    }
+
+    public async rejectAllProjectEntries(){
+        let index = 0;
+        let eHandles = await this.rejectButton.elementHandles();
+        while(await this.rejectButton.count() > 0){
+            await this.enterCommentforTimesheetEntry(index, "Sample comment");
+            await eHandles[index].click();
+            index++;
+        }
+    }
+
+    public async enterCommentforTimesheetEntry(index: number, comment :string){
+        await this.commentBox.nth(index).fill(comment);
+    }
+
+    public async getResubmissionDialogText(){
+        return await this.resubmissionDialogHeading.innerText();
+    }
+
+    public async selectResubmissionAllowOption(option:boolean){
+        if(option){
+            await this.resubmissionAllowButton("Yes, allow");
         }
     }
 }
--- tests/TimeSheetManagement/Pendingapprovals.spec.ts
+++ tests/TimeSheetManagement/Pendingapprovals.spec.ts
@@ -1,8 +1,9 @@
-import { test, Page } from "@playwright/test";
+import { test, Page, expect } from "@playwright/test";
 import { PendingApprovals } from "../../pages/PendingApprovals";
 import { LoginPage } from "../../pages/LoginPage";
 import testData from "../../testData/testData.json";
 import { TimesheetApi } from "../../api/TimesheetManagement/TimesheetApi";
+import {TIMESHEET_RESUBMISSION_DIALOGBOX_HEADING} from "../../utils/constants";
 
 
 let PAPage : PendingApprovals;
@@ -23,6 +24,16 @@ test.describe("Pending Approvals", () =>{
         await PAPage.approveAllProjectEntries();
         await PAPage.clickButton("Submit");
     })
-    
+
+    test("RejectTimeSheet", async () =>{
+        await TimesheetApi.submit();
+        await loginPage.validLogin(testData.ReportingManager.UserEmail, testData.SuperUser.UserPassword);
+        await PAPage.navigateToTab("Timesheet Management", "Pending approvals");
+        await PAPage.clickOnViewButtonforFirstResult();
+        await PAPage.rejectAllProjectEntries();
+        await PAPage.clickButton("Submit");
+        expect(await PAPage.getResubmissionDialogText()).toEqual(TIMESHEET_RESUBMISSION_DIALOGBOX_HEADING);
+        await PAPage.selectResubmissionAllowOption(true);
+    })
     
 });
